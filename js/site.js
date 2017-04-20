f323306d52ac18571abe2a6acf06bf9893ca6d6c
Noname <idonthave@email.com>
2017-04-20 22:30:06 +0700

    Update

--- js/site.js
+++ js/site.js
@@ -1,1 +1,48 @@
-﻿
+﻿var thumbnail = [
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/1.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/2.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/3.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/4.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/5.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/6.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/7.svg',
+   'https://rawgit.com/k7ae95/as5g/d001/img/thumbnail/empty/8.svg'
+];
+
+var month = [
+   'Jan', 'Feb', 'Mar', ' Apr', ' May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+$(document).ready(function () {
+   init();
+});
+
+function init() {
+   initBlogIndexPost();
+}
+
+var initBlogIndexPost = function () {
+   var max = $(thumbnail).length;
+
+   $('.blog-index .timestamp').each(function (index, value) {
+      //m/d/yyyy
+      var date = $(this).find('.content').text().split('/');
+
+      $(this).find('.day').text(date[1]);
+      $(this).find('.month').text(month[parseInt(date[0] - 1)]);
+      $(this).find('.year').text(date[2]);
+   });
+
+   $('.blog-index .thumbnail').each(function (index, value) {
+      var url;
+
+      if ($(this).hasClass('no-image')) {
+         url = thumbnail[Math.floor(Math.random() * max)];
+      }
+      else {
+         url = $(this).attr('thumbnailUrl');
+         url = url.replace('/s72-c/', '/s480/');
+      }
+
+      $(this).css('background-image', 'url(' + url + ')');
+   });
+}
